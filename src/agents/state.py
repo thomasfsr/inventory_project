39f0d5c399fec00fe4b36696b82efe4c13df5198
Thomás Freire <thomas.fsr@gmail.com>
2024-12-31 19:14:30 -0300

    nodes

---
+++ src/agents/state.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from typing_extensions import TypedDict
+from pydantic import BaseModel, Field
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from sqlalchemy import text, inspect
+from langgraph.graph import StateGraph, END
+
+class AgentState(TypedDict):
+    question: str
+    sql_query: str
+    query_result: str
+    query_rows: list
+    current_user: str
+    attempts: int
+    relevance: str
+    sql_error: bool
